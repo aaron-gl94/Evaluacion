ce9a4b6069a466144921d2687a5ca2c8ca1c6fb0
Aarón García Lezama <aaron.garcia.94@icloud.com>
2024-04-05 21:44:25 -0600

    Fix: Se aplica Refactor al paquete [com.academia.evaluacion];

--- src/main/java/com/academia/evaluacion/EvaluacionApplication.java
+++ src/main/java/com/academia/evaluacion/EvaluacionApplication.java
@@ -1,4 +1,4 @@
-package com.evaluacion.evaluacion;
+package com.academia.evaluacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
