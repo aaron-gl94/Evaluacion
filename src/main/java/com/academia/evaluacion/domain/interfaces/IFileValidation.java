9d402a66966dbf621bf26162221837d180487790
Aarón García Lezama <aaron.garcia.94@icloud.com>
2024-04-06 19:49:23 -0600

    Fix: Correcciones en la capa de dominio [domain].

---
+++ src/main/java/com/academia/evaluacion/domain/interfaces/IFileValidation.java
@@ -0,0 +1,12 @@
+package com.academia.evaluacion.domain.interfaces;
+
+import com.academia.evaluacion.domain.exceptions.FileException;
+
+public interface IFileValidation {
+	
+	String	fileNameRegex	= "^([a-zA-Z0-9]{10})(?:_(anverso|reverso|selfie))?_([0-9]{4}-[0-9]{2}-[0-9]{2})\\.(jpg|jpeg|png)$";
+	String	fileIdRegex		= "^([a-zA-Z0-9]{10})";
+
+	void	doValidation(String file) throws FileException;
+
+}
--- src/main/java/com/academia/evaluacion/domain/services/FileValidation.java
+++ src/main/java/com/academia/evaluacion/domain/services/FileValidation.java
@@ -5,20 +5,19 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Service;
 
 import com.academia.evaluacion.domain.exceptions.FileException;
-import com.academia.evaluacion.domain.interfaces.IValidate;
+import com.academia.evaluacion.domain.interfaces.IFileValidation;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
-public class FileValidation implements IValidate {
+public class FileValidation implements IFileValidation {
 	@Override
 	public void doValidation (String file) throws FileException {
 		log.info("Archivo recibido:{}", file);
@@ -26,80 +25,85 @@ public class FileValidation implements IValidate {
 		this.fileNameRegex_Validation(file);
 		this.fileExtension_Validation(file);
 		
-		// Separando nomenclatura del archivo
 		String[]	fileNameSlice	= this.getNameSlices(file);
 		String		fileDateString	= fileNameSlice[2].substring(0, fileNameSlice[2].indexOf("."));
 		
-		this.fileUUID_Validation(fileNameSlice[0]);
-		this.fileImage_Validation(fileNameSlice[1]);
+		this.fileId_Validation(fileNameSlice[0]);
+		this.fileDoc_Validation(fileNameSlice[1]);
 		this.fileDate_Validation(fileDateString);
 	}
 	
-	private String[] getNameSlices(String fileName) throws FileException {
+	public String[] getNameSlices(String fileName) throws FileException {
 		
 		String[] filenameSplices = fileName.split("_");
+		
+		log.info("Segmentación: {}", filenameSplices.length);
 		
 		if (filenameSplices.length == 3) {
 			log.info("[getNameSlices]: OK");
 			return filenameSplices;
 			
 		} else {
+			log.error("[getNameSlices]: El nombre del archivo no fue segmentado correctamente.");
 			throw new FileException("[getNameSlices]: El nombre del archivo no fue segmentado correctamente.");
 		}
 	}
 	
 	
-	private void fileNameRegex_Validation(String filename) throws FileException {
-		Pattern patron	= Pattern.compile(IValidate.fileNameRegex);
+	public void fileNameRegex_Validation(String filename) throws FileException {
+		Pattern patron	= Pattern.compile(fileNameRegex);
 	    Matcher matcher = patron.matcher(filename);
 	    
 	    Boolean result	= matcher.matches();
 	    
 	    if (Boolean.FALSE.equals(result)) {
-            throw new FileException("[filenameRegexValidation]: El nombre del archivo no cumple con el patrón indicado.");
+	    	log.error("[filenameRegex_Validation]: El nombre del archivo no cumple con el patrón indicado.");
+            throw new FileException("[filenameRegex_Validation]: El nombre del archivo no cumple con el patrón indicado.");
         }
 	    
-	    log.info("[fileNameRegexValidation]: OK");
+	    log.info("[fileNameRegex_Validation]: OK");
 	}
 	
-	private void fileExtension_Validation(String fileName) throws FileException {
+	public void fileExtension_Validation(String fileName) throws FileException {
 		  Set<String> validExtensions = new HashSet<>(Arrays.asList("jpg", "jpeg", "png"));
 		  
 		  String extension = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
 		  
-		  log.info("[fileExtensionValidation]: {}", extension);
+		  log.info("[fileExtension_Validation]: {}", extension);
 		  
-		  if (validExtensions.contains(extension)) {
-			  log.info("[fileExtensionValidation]: La extension del archivo es invalida.");
-			  throw new FileException("[fileExtensionValidation]: La extension del archivo es invalida.");
+		  if (!validExtensions.contains(extension)) {
+			  log.error("[fileExtension_Validation]: La extension del archivo es invalida.");
+			  throw new FileException("[fileExtension_Validation]: La extension del archivo es invalida.");
 		  }
 	}
 	
-	private void fileUUID_Validation(String fileName) throws FileException {
-		
-		try {
-			String uuidResult = UUID.fromString(fileName).toString();
-			log.info("[fileUUIDValidation]: {}", uuidResult);
-			
-		} catch(IllegalArgumentException e) {
-			log.error("[fileUUIDValidation]: Invalido. | Error: {}", e);
-			throw new FileException("[fileUUIDValidation]: Invalido. | Error: "+e);
-		}
+	public void fileId_Validation(String fileName) throws FileException {
+		Pattern patron	= Pattern.compile(IFileValidation.fileIdRegex);
+	    Matcher matcher = patron.matcher(fileName);
+	    
+	    Boolean result	= matcher.matches();
+	    
+	    if (Boolean.FALSE.equals(result)) {
+	    	log.error("[fileIdRegex_Validation]: El Id del archivo no cumple con el patrón indicado.");
+	    	throw new FileException("[fileIdRegex_Validation]: El Id del archivo no cumple con el patrón indicado.");
+        }
+	    
+	    log.info("[fileIdRegex_Validation]: OK");
 		
 	}
 	
-	private void fileImage_Validation(String filename) throws FileException {
+	public void fileDoc_Validation(String filename) throws FileException {
 	    Set<String> validDocs = new HashSet<>(Arrays.asList("anverso", "reverso", "selfie"));
 
 	    if (validDocs.contains(filename)) {
-	    	log.info("[fileCustomerDocValidation]: {}", validDocs.contains(filename));
+	    	log.info("[fileDoc_Validation]: {}", validDocs.contains(filename));
 	    } else {
-	    	log.error("[fileCustomerDocValidation]: Invalido");
-			throw new FileException("[fileCustomerDocValidation]: Tipo de documento inválido. | Error: " + filename);
+	    	log.error("[fileDoc_Validation]: Invalido");
+			throw new FileException("[fileDoc_Validation]: Tipo de documento inválido. | Error: " + filename);
 	    }
 	}
 	
-	private void fileDate_Validation(String fileDate) throws FileException {
+	public void fileDate_Validation(String fileDate) throws FileException {
 	    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 	    sdf.setLenient(false);
 	    
@@ -107,7 +111,8 @@ public class FileValidation implements IValidate {
 	    	Date parsedDate = sdf.parse(fileDate);
 	    	sdf.format(parsedDate);
 		} catch (Exception e) {
-			throw new FileException("[fileDateReviewValidation]: Fecha del nombre de documento inválida. | Error: " + e);
+			log.error("[fileDate_Validation]: Fecha del nombre de documento inválida. | Error: " + e);
+			throw new FileException("[fileDate_Validation]: Fecha del nombre de documento inválida. | Error: " + e);
 		}
 	}
 	
